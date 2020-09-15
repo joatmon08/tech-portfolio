24c05f1160220d8efe2530a999e3ecace15d8343
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2020-09-15 09:16:47 -0400

    Fix baseUrl

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -16,7 +16,7 @@ const siteConfig = {
     tagline: 'Continuously improving, aspiring technologist.',
     profilePhoto: '/img/profile.png',
     url: 'https://joatmon08.github.io', // Your website URL
-    baseUrl: '/tech-portfolio', // Base URL for your project */
+    baseUrl: '/tech-portfolio/', // Base URL for your project */
     // For github.io type URLs, you would set the url and baseUrl like:
     //   url: 'https://facebook.github.io',
     //   baseUrl: '/test-site/',
