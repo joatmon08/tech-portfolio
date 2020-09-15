4d1b79b9fdfb742be15ee79b7285dbaaa07edbb6
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2020-09-15 17:21:34 -0400

    [ci skip] Publish new dockerfile

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,7 @@ WORKDIR /app/website
 EXPOSE 3000 35729
 COPY ./docs /app/docs
 COPY ./website /app/website
+COPY ./publish.sh /usr/local/bin
 RUN yarn install
 
 CMD ["yarn", "start"]
---
+++ publish.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+set -e
+
+INPUT_BRANCH=${INPUT_BRANCH:-gh-pages}
+INPUT_DIRECTORY=${INPUT_DIRECTORY:-'website/build/tech-portfolio'}
+
+echo "Push to branch $INPUT_BRANCH";
+[ -z "${GITHUB_TOKEN}" ] && {
+    echo 'Missing input "GITHUB_TOKEN=${{ secrets.GITHUB_TOKEN }}".';
+    exit 1;
+};
+
+cp -r ${INPUT_DIRECTORY} /gh-pages
+
+cd /gh-pages
+git init
+git config --local user.email "action@github.com"
+git config --local user.name "GitHub Action"
+git commit -m "Push to Github Pages" -a
+
+remote_repo="https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+
+git push "${remote_repo}" HEAD:${INPUT_BRANCH} --follow-tags --force
